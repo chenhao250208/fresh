9dd040e0d3e11f945cdabfdbfe53fe6675f1ddbe
liang lei <18321299378@163.com>
2019-05-08 23:32:48 +0800

    部分Mapper、Entity

--- src/main/java/com/store/fresh/entity/Product.java
+++ src/main/java/com/store/fresh/entity/Product.java
@@ -1,4 +1,15 @@
 package com.store.fresh.entity;
 
+import lombok.Data;
+
+@Data
 public class Product {
+    private String productId;
+    private String productName;
+    private String category;
+    private float cost;
+    private float price;
+    private int deposit;
+    private String uint;
+    private String scale;
 }
---
+++ src/main/java/com/store/fresh/mapper/ProductMapper.java
@@ -0,0 +1,25 @@
+package com.store.fresh.mapper;
+
+import com.store.fresh.entity.Product;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ProductMapper {
+    public List<Product> findAll();
+
+    int deleteByPrimaryKey(String productId);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(String productId);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+
+}
